21ba1b7155970cc13e84db57dd9f9f8cae1e7ed6
iamkunalhere <iamkunalhere@gmail.com>
2020-03-28 03:13:11 +0530

    ADDED: Email address test

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -3,11 +3,14 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     static String patternForName="^[A-Z][a-z]{2}[a-z]*$";
-
+    static String patternForEmail="^[a-z][a-z0-9]*[.+-]?[a-z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]{2,3}([.]{1}[a-z]{2,3})?$";
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
     }
     public boolean checkName(String Name) {
         return Pattern.matches(patternForName,Name);
+    }
+    public boolean checkEmail(String Email) {
+        return Pattern.matches(patternForEmail,Email);
     }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -6,13 +6,18 @@ import static org.junit.Assert.*;
 public class UserRegistrationTest {
     UserRegistration object=new UserRegistration();
     @Test
-    public void name_is_validThenTrue(){
-        boolean Name=object.checkName("name");
+    public void name_is_validThenTrue() {
+        boolean Name=object.checkName("Name");
         Assert.assertTrue(Name);
     }
     @Test
-    public void name_is_invalidThenFalse(){
+    public void name_is_invalidThenFalse() {
         boolean Name=object.checkName("name");
         Assert.assertFalse(Name);
+    }
+    @Test
+    public void email_is_validThenTrue() {
+        boolean Email=object.checkEmail("iamkunalhere@gmail.com");
+        Assert.assertTrue(Email);
     }
 }
